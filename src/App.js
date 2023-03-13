3fd8e4dec7da577c884ebb4f5315eef3ecac03b7
kry23 <kkorkmaz1881@gmail.com>
2023-03-13 17:57:34 +0300

    ex2 sol

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
-function App() {}
+import React from "react";
+class App extends React.Component {
+  state = {};
+  handleName(name) {
+    return <h1>Hello, {name}</h1>;
+  }
+  render() {
+    return this.handleName("kry");
+  }
+}
 
 export default App;
