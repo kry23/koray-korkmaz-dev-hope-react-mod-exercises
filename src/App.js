84d9b4b66c9a2fdceeccc61e5966e8a2e0802f96
kry23 <kkorkmaz1881@gmail.com>
2023-03-13 18:41:25 +0300

    ex-cmp-8 sol.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { Hello } from "./Hello";
 import React from "react";
 export class App extends React.Component {
   render() {
-    return <Hello />;
+    return (
+      <div>
+        <Hello /> <Hello />;
+        <Hello />
+      </div>
+    );
   }
 }
