11eda2df51b10ca08e0866f16398a5b99194724b
kry23 <kkorkmaz1881@gmail.com>
2023-03-13 18:44:06 +0300

    ex-cmp-9

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Hello } from "./Hello";
+import { Message } from "./Message";
 import React from "react";
 export class App extends React.Component {
   render() {
     return (
       <div>
-        <Hello /> <Hello />;
-        <Hello />
+        <Hello></Hello>
+        <Message></Message>
       </div>
     );
   }
--- src/Hello.js
+++ src/Hello.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { Message } from "./Message";
+
 export class Hello extends React.Component {
   state = {};
   render() {
     return (
       <div>
         <h1>Hello, World!</h1>
-        <Message />
       </div>
     );
   }
