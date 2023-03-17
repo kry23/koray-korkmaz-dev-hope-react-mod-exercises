adb90f004fbd7416b24908bbaae2b04ef1141dda
kry23 <kkorkmaz1881@gmail.com>
2023-03-17 11:24:06 +0300

    cond-5 sol.

--- src/Age.jsx
+++ src/Age.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-export class Age extends React.Component {
+class Age extends React.Component {
   state = {};
   render() {
-    return <p>Your age is {this.props.age}.</p>;
+    const { age } = this.props;
+
+    return (
+      <div>
+        {age > 18 ? <p>Your age is {age}.</p> : <p>You are very young!</p>}
+      </div>
+    );
   }
 }
 
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -7,18 +7,17 @@ export class Welcome extends React.Component {
 
   state = {};
   render() {
-    let age = 64;
-    let ageComponent = null;
+    let age = 17;
+    // let ageComponent = null;
 
-    if (age >= 18 && age < 65) {
-      ageComponent = <Age age={age}></Age>;
-    }
-    console.log(this.props.name);
+    // if (age >= 18 && age < 65) {
+    //   ageComponent = <Age age={age}></Age>;
+    // }
 
     return (
       <>
         {this.props.name === "John" && <p>Welcome, {this.props.name}!</p>}
-        {ageComponent}
+        <Age age={age}></Age>
       </>
     );
   }
