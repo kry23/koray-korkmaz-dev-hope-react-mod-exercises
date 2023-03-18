7498d8d4ebd672a781e816e1ac94cb70e5037c6e
kry23 <kkorkmaz1881@gmail.com>
2023-03-18 14:11:46 +0300

    fnc-cmp-1 sol.

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,30 +1,14 @@
 import React from "react";
 import Age from "./Age";
-import ClickCounter from "./ClickCounter";
-import ClickTracker from "./ClickTracker";
-// import Counter from "./Counter";
-export class Welcome extends React.Component {
-  static defaultProps = {
-    name: "John",
-  };
-
-  state = {};
-  render() {
-    let age = 1;
-    // let ageComponent = null;
-
-    // if (age >= 18 && age < 65) {
-    //   ageComponent = <Age age={age}></Age>;
-    // }
-
-    return (
-      <div className="welcome">
-        <p className="text-2xl font-bold">Welcome, {this.props.name}!</p>
-        <Age age={age}></Age>
-        {/* <Counter initVal={11} incInt={1000} incVal={3}></Counter> */}
-        {/* <ClickCounter />
-        <ClickTracker /> */}
-      </div>
-    );
-  }
+export function Welcome({ name = "John" }) {
+  let age = 1;
+  return (
+    <div className="welcome">
+      <p className="text-2xl font-bold">Welcome, {name}!</p>
+      <Age age={age}></Age>
+      {/* <Counter initVal={11} incInt={1000} incVal={3}></Counter> */}
+      {/* <ClickCounter />
+    <ClickTracker /> */}
+    </div>
+  );
 }
